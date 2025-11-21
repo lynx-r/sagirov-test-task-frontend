42080a82c2d38522e7d91d51af5a4bdb81d1d851
Aleksey P <aleksey.p10n@gmail.com>
2025-11-21 16:58:51 +0300

    fix seo

--- app/app.vue
+++ app/app.vue
@@ -7,9 +7,8 @@ useHead({
   },
 })
 
-const title = 'Nuxt Starter Template'
-const description =
-  'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'Test task'
+const description = 'Sagirov.com test task'
 
 useSeoMeta({
   title,
