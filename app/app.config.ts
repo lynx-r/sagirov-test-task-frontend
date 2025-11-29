06af5c45070c0c255d05e5a5b066f3ba05a64533
Aleksey P <aleksey.p10n@gmail.com>
2025-11-29 16:54:12 +0300

    fix: remove horiz scroll

--- app/app.config.ts
+++ app/app.config.ts
@@ -5,22 +5,12 @@ export default defineAppConfig({
       neutral: 'neutral',
     },
     button: {
-      // rounded: 'rounded-lg',
-      // default: {
-      //   base: 'rounded-full',
-      // },
       slots: {
         base: 'rounded-full',
       },
     },
     container: {
-      // base: '!max-w-7xl', // Устанавливает максимальную ширину для всех UContainer и UHeader
-      // padding: 'p-4 sm:p-6 lg:p-8 mx-0',
-    },
-  },
-  header: {
-    slots: {
-      // container: 'sm:px-1 lg:px-1 px-1',
+      base: 'overflow-hidden',
     },
   },
 })
